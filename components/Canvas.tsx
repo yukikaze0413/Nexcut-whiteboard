0774fa106725bc089e2067b4d5a13395b5c1d752
starsgo <1526560658@qq.com>
2025-07-30 11:22:02 +0800

    2 <g>

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -444,7 +444,8 @@ const Canvas: React.FC<CanvasProps> = ({ items, layers, selectedItemId, onSelect
                         {items.filter(item => item.layerId === layer.id).map(item => (
                             <g 
                                 key={item.id} 
-                                transform={`translate(${'x' in item ? item.x : 0}, ${'y' in item ? item.y : 0})`}
+                                // gxx: avoid two times <g>  the other <g> is in components/renders/imageRender.tsx
+                                // transform={`translate(${'x' in item ? item.x : 0}, ${'y' in item ? item.y : 0})`}
                                 data-item-id={item.id}
                                 className={activeTool === ToolType.SELECT ? 'cursor-pointer' : ''}
                             >
